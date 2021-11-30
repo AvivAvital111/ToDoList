40445527c7d820a9478308f94862cfe55d8439d4
avi <avivavital111@gmail.com>
2021-11-30 16:58:26 +0200

    added popup

--- ToDoList.js
+++ ToDoList.js
@@ -9,11 +9,24 @@ class ToDoItem {
 }
 
 
+function popup() {
+    popup.classList.remove("hidden");
+    popup.classList.toggle("show");
+    document.body.classList.toggle("blur_body");
+}
+
+function confirmPopup(answer) {
+    (answer) ? submitTask(task_description=document.getElementById("task_input").value): null;
+    popup.classList.remove("show");
+    popup.classList.toggle("hidden");
+    document.body.classList.remove("blur_body");
+}
+
 function submitTask(task_description) {
     let task = new ToDoItem(task_description)
     toDoItems.push(task);
     updateTaskList(task);
-    task_description=document.getElementById("task_input").value = ""
+    document.getElementById("task_input").value = "";
 }
 
 function deleteTask(task) {
@@ -31,7 +44,7 @@ function deleteTasks() {
 function createCheckbox() {
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox'
+    checkbox.className = 'checkbox';
     return checkbox
 }
 
@@ -40,10 +53,10 @@ function deleteSingleTask(task_id) {
 }
 
 function deleteTaskFromDeleteButton(task, task_id) {
-    if (task.id == task_id) {
+    if (task.id === task_id) {
         document.getElementById('tasks_table').removeChild(document.getElementById(task.entry.id))
     }
-    return task.id != task_id
+    return task.id !== task_id
 }
 
 
@@ -51,41 +64,55 @@ function createDeleteTaskButton(task_id) {
     let delete_button = document.createElement('button');
     delete_button.textContent = "X"
     delete_button.className = 'delete_button'
-    delete_button.onclick = () => {deleteSingleTask(task_id)}
+    delete_button.onclick = () => deleteSingleTask(task_id)
     return delete_button
 }
 
 function createEntry(task) {
     let entry = document.createElement('tr');
-    let mark = document.createElement('td')
-    let task_description = document.createElement('td')
-    let delete_task = document.createElement('td')
-    mark.prepend(task.checkbox)
+    let mark = document.createElement('td');
+    let task_description = document.createElement('td');
+    let delete_task = document.createElement('td');
+    mark.prepend(task.checkbox);
     task_description.textContent = task.taskDesctiption;
-    task_description.className = 'task_desc'
-    delete_task.prepend(createDeleteTaskButton(task.id))
+    task_description.className = 'task_desc';
+    delete_task.prepend(createDeleteTaskButton(task.id));
     entry.id = "entry" + task.id;
-    entry.className = 'TaskEntry'
-    entry.append(mark, task_description, delete_task)
+    entry.className = 'TaskEntry';
+    entry.append(mark, task_description, delete_task);
     return entry
 }
 
 
 
 function updateTaskList(task) {
-    task.checkbox = createCheckbox(task.id)
-    let entry = createEntry(task)
+    task.checkbox = createCheckbox(task.id);
+    let entry = createEntry(task);
     document.getElementById('tasks_table').append(entry);
     task.entry = entry;
 }
 
-document.getElementById('submit_task').onclick = () => {submitTask(task_description=document.getElementById("task_input").value)}
-document.getElementById('delete').onclick = deleteTasks
+// document.getElementById('submit_task').onclick = () => {submitTask(task_description=document.getElementById("task_input").value)}
+document.getElementById('submit_task').onclick = popup;
+document.getElementById('delete').onclick = deleteTasks;
+var popup = document.getElementById("popup");
+popup.classList.toggle("hidden");
+
+document.getElementById('yesPopup').onclick = () => {confirmPopup(true)};
+document.getElementById('noPopup').onclick = () => {confirmPopup(false)};
+
+
 
 var input = document.getElementById('task_input')
 
 input.addEventListener("keyup", function(event) {
     if (event.key == "Enter") {
-      document.getElementById("submit_task").click();
+        if (popup.classList.contains("show")) {
+            document.getElementById('yesPopup').click();
+        }
+        else
+        {
+            document.getElementById("submit_task").click();
+        }
     }
   });
